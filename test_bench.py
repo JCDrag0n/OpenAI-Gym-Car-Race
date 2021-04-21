931eb0750003171b6052d2b5b993ce525bbf1972
Taaseen Ali <taaseen.ali89@gmail.com>
2021-04-21 12:51:50 -0400

    added reward functions

--- test_bench.py
+++ test_bench.py
@@ -8,6 +8,47 @@ from gym_car_race.training_utils import TensorboardCallback, constant_schedule, 
 from gym_car_race.config import cfg
 
 
+def get_exp(beta):
+    return lambda x : (1/beta) * math.exp(-1*x/beta)
+
+def midline_index(obs):
+    sensor_data = [entry[3] for entry in obs]    
+    exp = get_exp(len(sensor_data)//4) 
+    
+    total = 0
+    for i in range(len(sensor_data)//2):        
+        diff = abs(sensor_data[i] - sensor_data[-1*i -1])
+        length = sensor_data[i] + sensor_data[-1*i -1]
+        percent_diff = diff/length if length else 1
+        weighted_diff = exp(i) * percent_diff
+        total += weighted_diff
+    
+    return 1 - total
+
+@Car.reward_function
+def reward_func(car):    
+    # TODO: 
+    # - try increasing min speed and decreasing turn radius
+    # - midline index
+    # - try rewards depending on action taken instead of state
+    # - try including distance from end
+    reward = 0
+    if car.crashed:
+        reward = car.config["reward"]["crash_reward"]
+    elif car.has_finished:
+        print('finished!')
+        reward += 100
+    else:
+        curr = car.current_tile()
+        if curr not in car.traveled:
+            reward = car.config["reward"]["new_tile_reward"] * midline_index(car.sensors)
+        elif car.speed <= car.config["reward"]["min_speed"]:
+            reward = car.config["reward"]["same_tile_penalty"]
+        else:
+            reward = car.config["reward"]["same_tile_reward"]
+    return reward
+
+
 # Specify folders to save models/logs in
 
 model_dir = "models"
